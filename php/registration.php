0d3f11dd4d6003550708c79dbce394653496600b
HippieHosenHustler <scharfe.edwin@gmail.com>
2019-02-28 17:45:23 +0100

    Saves users

--- php/registration.php
+++ php/registration.php
@@ -2,8 +2,30 @@
 
 include 'databaseOperations.php';
 
-$gender = $_POST['gender'];
-$username = $_POST['user'];
-$password = $_POST['password'];
-$address = $_POST['address'];
+// Are values given?
+if (isset($_POST['gender'])){
+    $gender = $_POST['gender'];
+} else die("Gender required!");
 
+if (isset($_POST{'user'})) {
+    $username = $_POST['user'];
+} else die("Username required!");
+
+if (isset($_POST['password'])) {
+    $password = $_POST['password'];
+} else die("Password required!");
+
+if (isset($_POST['address'])) {
+    $address = $_POST['address'];
+}
+
+// Is username taken?
+if (isUserTaken($username)) {
+    die("Username is taken");
+}
+
+// hash password
+$passHash = md5($password);
+
+// save User
+saveUser($username, $passHash, $gender, $address);
