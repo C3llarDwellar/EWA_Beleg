1d25cb996b7dd001edce6c1d7fc152aeeaca5404
HippieHosenHustler <scharfe.edwin@gmail.com>
2019-03-04 21:52:10 +0100

    font-size changeable

--- javascript/pageFunctions.js
+++ javascript/pageFunctions.js
@@ -17,4 +17,29 @@ $(document).ready(function () {
     });
 
     admin();
+
+    let biggerFontButton = $('#fontPlus');
+    let normalFontButton = $('#fontNormal');
+    let smallerFontButton = $('#fontMinus');
+
+    biggerFontButton.on('click', function () {
+        $('nav').css("font-size", "150%");
+        $('footer').css("font-size", "150%");
+        $('main').css("font-size", "150%");
+        $('.modal').css("font-size", "150%");
+    });
+
+    normalFontButton.click(function () {
+        $('nav').css("font-size", "100%");
+        $('footer').css("font-size", "100%");
+        $('main').css("font-size", "100%");
+        $('.modal').css("font-size", "100%");
+    });
+
+    smallerFontButton.click(function () {
+        $('nav').css("font-size", "50%");
+        $('footer').css("font-size", "50%");
+        $('main').css("font-size", "50%");
+        $('.modal').css("font-size", "50%");
+    })
 });
